7f6123c7cf37fd02ccc87b1b0bb4e94a0bd98055
frectonz <fraol0912@gmail.com>
2025-04-18 22:50:11 +0300

    chore: big bang

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
+    let
+      forAllSystems =
+        fn:
+        let
+          systems = [ "x86_64-linux" "aarch64-darwin" ];
+          overlays = [ (import rust-overlay) ];
+        in
+        nixpkgs.lib.genAttrs systems (
+          system:
+          fn (
+            import nixpkgs {
+              inherit system overlays;
+            }
+          )
+        );
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.just
+            pkgs.bacon
+            pkgs.httpie
+            pkgs.cargo-expand
+            pkgs.rust-analyzer
+            pkgs.rust-bin.stable.latest.default
+          ];
+        };
+      });
+
+      formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
+    };
+}
---
+++ src/main.rs
@@ -0,0 +1,94 @@
+use indexmap::IndexMap;
+use serde::Deserialize;
+
+type Str = Box<str>;
+type List<T> = Box<[T]>;
+
+#[derive(Deserialize, Debug)]
+pub struct SchemaOrgDefinition {
+    #[serde(rename = "@context")]
+    pub context: IndexMap<Str, Str>,
+    #[serde(rename = "@graph")]
+    pub graph: List<Item>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ItemId {
+    #[serde(rename = "@id")]
+    pub item_id: String,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+pub enum ItemRef {
+    Single(ItemId),
+    List(List<ItemId>),
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+pub enum ItemType {
+    Single(Str),
+    List(List<Str>),
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+pub enum Text {
+    Simple(Str),
+    Tagged(LanguageText),
+}
+
+#[derive(Deserialize, Debug)]
+pub struct LanguageText {
+    #[serde(rename = "@language")]
+    pub language: Str,
+    #[serde(rename = "@value")]
+    pub value: Str,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Item {
+    #[serde(flatten)]
+    pub id: ItemId,
+
+    #[serde(rename = "@type")]
+    pub item_type: ItemType,
+    #[serde(rename = "rdfs:comment")]
+    pub comment: Text,
+    #[serde(rename = "rdfs:label")]
+    pub label: Text,
+
+    #[serde(rename = "schema:domainIncludes")]
+    pub domain_includes: Option<ItemRef>,
+    #[serde(rename = "schema:isPartOf")]
+    pub is_part_of: Option<ItemId>,
+    #[serde(rename = "schema:rangeIncludes")]
+    pub range_includes: Option<ItemRef>,
+    #[serde(rename = "schema:source")]
+    pub source: Option<ItemRef>,
+    #[serde(rename = "schema:contributor")]
+    pub contributor: Option<ItemRef>,
+    #[serde(rename = "rdfs:subClassOf")]
+    pub sub_class_of: Option<ItemRef>,
+    #[serde(rename = "schema:supersededBy")]
+    pub superseded_by: Option<ItemId>,
+    #[serde(rename = "rdfs:subPropertyOf")]
+    pub sub_property_of: Option<ItemRef>,
+    #[serde(rename = "owl:equivalentProperty")]
+    pub equivalent_property: Option<ItemId>,
+    #[serde(rename = "schema:inverseOf")]
+    pub inverse_of: Option<ItemId>,
+    #[serde(rename = "skos:closeMatch")]
+    pub close_match: Option<ItemRef>,
+    #[serde(rename = "skos:exactMatch")]
+    pub exact_match: Option<ItemId>,
+}
+
+fn main() -> color_eyre::Result<()> {
+    let file = std::fs::read_to_string("schema/schemaorg-current-https.jsonld")?;
+    let schema: SchemaOrgDefinition = serde_json::from_str(&file)?;
+    dbg!(schema);
+
+    Ok(())
+}
