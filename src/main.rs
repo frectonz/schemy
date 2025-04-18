a078fb445b5ee3489a2964d4bba1ba01097a9066
frectonz <fraol0912@gmail.com>
2025-04-18 23:53:26 +0300

    feat: add all the types

--- src/main.rs
+++ src/main.rs
@@ -54,35 +54,44 @@ pub struct Item {
 
     #[serde(rename = "@type")]
     pub item_type: ItemType,
-    #[serde(rename = "rdfs:comment")]
-    pub comment: Text,
     #[serde(rename = "rdfs:label")]
     pub label: Text,
-
-    #[serde(rename = "schema:domainIncludes")]
-    pub domain_includes: Option<ItemRef>,
-    #[serde(rename = "schema:isPartOf")]
-    pub is_part_of: Option<ItemId>,
-    #[serde(rename = "schema:rangeIncludes")]
-    pub range_includes: Option<ItemRef>,
-    #[serde(rename = "schema:source")]
-    pub source: Option<ItemRef>,
-    #[serde(rename = "schema:contributor")]
-    pub contributor: Option<ItemRef>,
+    #[serde(rename = "rdfs:comment")]
+    pub comment: Text,
     #[serde(rename = "rdfs:subClassOf")]
     pub sub_class_of: Option<ItemRef>,
-    #[serde(rename = "schema:supersededBy")]
-    pub superseded_by: Option<ItemId>,
     #[serde(rename = "rdfs:subPropertyOf")]
     pub sub_property_of: Option<ItemRef>,
-    #[serde(rename = "owl:equivalentProperty")]
-    pub equivalent_property: Option<ItemId>,
+
+    #[serde(rename = "schema:rdfs:label")]
+    pub rdfs_label: Option<Str>,
+
+    #[serde(rename = "schema:source")]
+    pub source: Option<ItemRef>,
+    #[serde(rename = "schema:sameAs")]
+    pub same_as: Option<ItemRef>,
+    #[serde(rename = "schema:isPartOf")]
+    pub is_part_of: Option<ItemId>,
     #[serde(rename = "schema:inverseOf")]
     pub inverse_of: Option<ItemId>,
+    #[serde(rename = "schema:contributor")]
+    pub contributor: Option<ItemRef>,
+    #[serde(rename = "schema:supersededBy")]
+    pub superseded_by: Option<ItemId>,
+    #[serde(rename = "schema:rangeIncludes")]
+    pub range_includes: Option<ItemRef>,
+    #[serde(rename = "schema:domainIncludes")]
+    pub domain_includes: Option<ItemRef>,
+
     #[serde(rename = "skos:closeMatch")]
     pub close_match: Option<ItemRef>,
     #[serde(rename = "skos:exactMatch")]
     pub exact_match: Option<ItemId>,
+
+    #[serde(rename = "owl:equivalentClass")]
+    pub equivalent_class: Option<ItemRef>,
+    #[serde(rename = "owl:equivalentProperty")]
+    pub equivalent_property: Option<ItemId>,
 }
 
 fn main() -> color_eyre::Result<()> {
